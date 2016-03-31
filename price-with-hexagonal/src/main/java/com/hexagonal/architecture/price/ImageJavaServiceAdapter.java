9c1d85c608cc9ac4ce235ff641e01d23e404b3d4
tmcconville <tylermcconville@gmail.com>
2016-03-31 13:58:11 -0500

    Add comments about location of adapters

--- price-with-hexagonal/src/main/java/com/hexagonal/architecture/price/ImageJavaServiceAdapter.java
+++ price-with-hexagonal/src/main/java/com/hexagonal/architecture/price/ImageJavaServiceAdapter.java
@@ -2,6 +2,9 @@ package com.hexagonal.architecture.price;
 
 import com.hexagonal.architecture.image.Image;
 
+// This adapter would not traditionally be in the same module as the business logic class
+// It should be with the driving application (in this case, the monolith)
+// For simplicity's sake, I combined them for the purposes of the presentation
 public class ImageJavaServiceAdapter implements ImagePort{
 
     public Image getImage(String id){
--- price-with-hexagonal/src/main/java/com/hexagonal/architecture/price/ImageMicroserviceAdapter.java
+++ price-with-hexagonal/src/main/java/com/hexagonal/architecture/price/ImageMicroserviceAdapter.java
@@ -10,7 +10,9 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
+// This adapter would not traditionally be in the same module as the business logic class
+// It should be with the driving application (in this case, the microservice)
+// For simplicity's sake, I combined them for the purposes of the presentation
 @Component
 public class ImageMicroserviceAdapter implements ImagePort{
 
